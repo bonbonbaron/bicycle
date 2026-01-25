24da9a00da3912a70298d9e3f9a99f6cd8de7d36
Michael <bonbonbaron@gmail.com>
2026-01-25 09:31:26 -0600

    rearranged nodes and edges to graph.h

--- include/Graph.h
+++ include/Graph.h
@@ -5,13 +5,54 @@
 #include <memory>
 #include <mutex>
 #include <functional>
+#include <optional>
 #include <vector>
 
-#include "Edge.h"
 #include <yaml-cpp/node/convert.h>
 #include "Entity.h"
+#include "ConditionRegistry.h"
       
 static std::mutex _nodeMut{};  // There'll only ever be one node active.
+
+
+// Edges are mapped by name, so there's no need for a name field.
+class Edge {
+  public:
+    void getEndpoint();
+    void setWeight( const int weight );
+    auto getWeight() const -> int;
+    void setEndpoint( const std::string& endpoint );
+    auto getEndpoint() const -> const std::string&;
+    void loadEndpoint() const;
+    void setCondition( const std::function<bool()>& func );
+  private:
+    int _weight{};                  // e.g. Number of random battles may be proportional to travel distance.
+    std::string _endpointFilename;  // Endpoint file's basename (no path or extension)
+    std::optional<std::function<bool()>> _condition{};  // e.g. "do you have at least one key?"
+};
+
+// Provide yaml-cpp library with template candidate for Edge's specific struct
+template<>
+struct YAML::convert<Edge> {
+  static YAML::Node encode(const std::string& rhs) { return YAML::Node(rhs); }
+  static bool decode(const YAML::Node& node, Edge& rhs) {
+    if (!node.IsMap()) {
+      return false;
+    }
+    if ( auto weight = node["weight"] ) {
+      rhs.setWeight( weight.as<int>() );
+    }
+    rhs.setEndpoint( node["endpointFilename"].as<std::string>() );
+    // If this edge is conditional
+    if ( auto conditionName = node["condition"] ) {
+      
+      auto& reg = ConditionRegistry::get();
+      auto& it = reg.at( conditionName.as<std::string>() );
+      rhs.setCondition( it );
+    }  // if this edge is conditional
+    return true;
+  }
+};
 
 namespace bicycle {  // prevent clash with YAML::Node
   class Node : public std::enable_shared_from_this<Node> {
