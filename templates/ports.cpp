9041082fac9d8dcc0a6b378ca13f29b92f3fca8c
Michael <bonbonbaron@gmail.com>
2026-02-06 11:38:29 -0600

    added ports template

---
+++ templates/ports.cpp
@@ -0,0 +1,9 @@
+#include <bicycle/Personality.h>
+
+extern "C" {
+  // old school ports needed for easier 
+  PortTypeRegistry::value_type ports[] {
+  };
+
+  int numPorts{ sizeof( ports ) / sizeof( ports[0] ) };
+}
