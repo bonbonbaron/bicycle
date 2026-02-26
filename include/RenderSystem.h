b44ee3a96fe7a828aa7b118614385fe53f9f5b17
Michael <bonbonbaron@gmail.com>
2026-02-26 09:10:11 -0600

    beautiful vectorized timer idea from Grok

--- include/RenderSystem.h
+++ include/RenderSystem.h
@@ -4,9 +4,11 @@
 // I like Grok's idea of storing everything in global arrays.
 // Archetypes are also a cool concept. Some systems ought to be allowed to process multiple thigns
 // together in order to avoid awkward messaging.
-class RenderSystem : public System<Image> {
+class BodySystem : public System<Body> {
   public:
-    RenderSystem();
+    BodySystem();
     void run() override;
     void setColor( const unsigned entityId, const Color color );
 };
+
+
